8ea96cf1e6395ed5a39dbcfe0c01eb10770b0325
sonnynajar <sonny.najar@gmail.com>
2020-01-15 14:49:56 -0800

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -3,7 +3,7 @@
 (require define-assets-from)
 
 ; I had to provide these out here for it to work
-(provide (all-from-out "stones.rkt"))
+;(provide (all-from-out "stones.rkt"))
 
 (also-for-asset-docs
  #:asset-modules ("pokemons.rkt"
